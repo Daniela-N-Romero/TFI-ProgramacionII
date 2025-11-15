e858f846de75ccb67bf783dd351d713be76999c4
donramones <agusrivarolaa00@gmail.com>
2025-11-15 19:31:17 -0300

    Servicio

--- src/Service/PedidoServiceImpl.java
+++ src/Service/PedidoServiceImpl.java
@@ -3,13 +3,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Service;
+import Dao.PedidoDAO;
+import Models.Pedido;
 import java.util.List;
 
 /**
  *
  * @author Daniela Nahir Romero
  */
-public class PedidoServiceImpl {
+
+//Implementación del servicio de negocio para la entidad Pedido
+//Capa intermedia entre la UI y el DAO que aplica validaciones de negocio complejas.
+public class PedidoServiceImpl implements GenericService<Pedido> {
+    private final PedidoDAO pedidoDAO;
+    
+    
+    private final EnvioServiceImpl envioServiceImpl;
+    
+    //contructor
+
+    public PedidoServiceImpl(PedidoDAO pedidoDAO, EnvioServiceImpl envioServiceImpl) {
+        if (pedidoDAO == null){
+            throw new IllegalArgumentException("PedidoDAO no puede ser null");
+        }
+        if (envioServiceImpl == null){
+            throw new IllegalArgumentException("EnvioServiceImpl no puede ser null");
+        }
+        this.pedidoDAO = pedidoDAO;
+        this.envioServiceImpl = envioServiceImpl;
+    }
+    
+    
+    //Inserta una nuevo pedido en la base de datos.
+    public void insertar(Pedido pedido) throws Exception {
+        validatePedido(pedido);
+        validateEnvio(pedido.getEnvio(), null);
+        
+        
+        
+        
+        
+        
+        
+       
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     
     
 }
